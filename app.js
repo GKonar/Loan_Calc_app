671d1240df33d39a790406e1ebf37f2969f026b4
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2019-03-04 14:10:27 +0100

    Finished Loan_Calc app

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // Lisen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+
+    // Show loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e) {
+function calculateResults() {
     console.log('Calculating...');
     // UI Vars
     const amount = document.getElementById('amount');
@@ -24,9 +34,50 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
-    } else {
-        console.log('Please check your numbers');
-    };
 
-    e.preventDefault();
+        // Show results
+        document.getElementById('results').style.display = 'block';
+
+        //Hide loader
+        document.getElementById('loading').style.display = 'none';
+
+    } else {
+        showError('Please check Your numbers');
+    };
 };
+
+
+// Show Error
+function showError(error) {
+    // Hide results
+    document.getElementById('results').style.display = 'none';
+
+    //Hide loader
+    document.getElementById('loading').style.display = 'none';
+
+    // Create a div
+    const errorDiv = document.createElement('div');
+
+    // Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+    // insertBefore inserts element - errorDiv, before the element passed in second parameter - heading
+
+    // Clear error after 3s
+    setTimeout(clearError, 3000);
+}
+
+// Clear Error
+
+function clearError() {
+    document.querySelector('.alert').remove();
+}
